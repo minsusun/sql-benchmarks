624a3f0f5bcca9afffe41517de1d82f5d9daa2e4
Minsu Sun <20790969+minsusun@users.noreply.github.com>
2025-02-24 15:15:48 +0900

    feat(ParquetConversion): add time profile

--- src/main/scala/com/ibm/crail/benchmarks/tests/ParquetConversion.scala
+++ src/main/scala/com/ibm/crail/benchmarks/tests/ParquetConversion.scala
@@ -5,11 +5,23 @@ import org.apache.spark.sql.SparkSession
 
 class ParquetConversion (val options: ParseOptions, spark:SparkSession) extends SQLTest(spark) {
   override def execute(): String = {
-
+    val textLoadStart = System.nanoTime();
     val txtDF = spark.read.text(options.getInputFiles()(0))
-    txtDF.write.parquet(s"${options.getInputFiles()(0)}.parquet")
+    val textLoadTimeString = "Text File Load Time: " + (System.nanoTime() - textLoadStart)/1000000 + "msec";
 
-    "Ran  ParquetConversion on " + options.getInputFiles()(0)
+    val parquetConversionStart = System.nanoTime();
+    txtDF.write.parquet(s"${options.getInputFiles()(0)}.parquet")
+    val parquetConversionTimeString = "Parquet Conversion Time: " + (System.nanoTime() - parquetConversionStart)/1000000 + "msec";
+
+    val parquetReadStart = System.nanoTime();
+    val _ = spark.read.parquet(s"${options.getInputFiles()(0)}.parquet")
+    val parquetReadTimeString = "Parquet Read Time: " + (System.nanoTime() - parquetReadStart)/1000000 + "msec";
+
+    "Ran  ParquetConversion on " +
+      options.getInputFiles()(0) +
+      "\n\t" + textLoadTimeString +
+      "\n\t" + parquetConversionTimeString +
+      "\n\t" + parquetReadTimeString
   }
 
   override def explain(): Unit = println(plainExplain())
