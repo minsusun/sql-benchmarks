12f93825d94479a0ec739d207425b7d6a8d424b5
Minsu Sun <20790969+minsusun@users.noreply.github.com>
2025-02-25 13:07:56 +0900

    feat: save graph as parquet and load

--- src/main/scala/com/ibm/crail/benchmarks/tests/ParquetConversion.scala
+++ src/main/scala/com/ibm/crail/benchmarks/tests/ParquetConversion.scala
@@ -1,81 +1,59 @@
 package com.ibm.crail.benchmarks.tests
 
 import com.ibm.crail.benchmarks.{Action, Noop, ParseOptions, SQLTest}
-import org.apache.spark.graphx.{Edge, Graph, GraphLoader}
+import org.apache.spark.graphx.impl.GraphImpl
+import org.apache.spark.graphx.{Edge, EdgeRDD, Graph, GraphLoader, VertexRDD}
 import org.apache.spark.sql.SparkSession
 
 class ParquetConversion (val options: ParseOptions, spark:SparkSession) extends SQLTest(spark) {
+
+  private var stepTime = System.nanoTime();
+
+  def step(StepName: String): String = {
+    val now = System.nanoTime();
+    val info = s"\n\t\t Step '" + StepName + s"': ${(now - stepTime)/1000000} ms"
+    stepTime = now;
+    info
+  }
+
   override def execute(): String = {
-    val DFLoadStart = System.nanoTime();
-    val txtDF = spark.read.text(options.getInputFiles()(0))
-    val DFLoadTimeString = "Text File(spark.read.text;DF) Load Time: " + (System.nanoTime() - DFLoadStart)/1000000 + "msec";
+    var s = "Ran Parquet Conversion on" + options.getInputFiles()(0)
 
-    val RDDLoadStart = System.nanoTime();
-    val txtRDD = spark.sparkContext.textFile(options.getInputFiles()(0))
-    val RDDLoadTimeString = "Text File(sparkContext.textFile;RDD) Load Time: " + (System.nanoTime() - RDDLoadStart)/1000000 + "msec";
-
-    val parquetConversionStart = System.nanoTime();
-    txtDF.write.parquet(s"${options.getInputFiles()(0)}.parquet")
-    val parquetConversionTimeString = "Parquet Conversion Time: " + (System.nanoTime() - parquetConversionStart)/1000000 + "msec";
-
-    val parquetReadStart = System.nanoTime();
-    val df = spark.read.parquet(s"${options.getInputFiles()(0)}.parquet")
-    val parquetReadTimeString = "Parquet Read Time: " + (System.nanoTime() - parquetReadStart)/1000000 + "msec";
-
-    val dfRDDConversionStart = System.nanoTime();
-    val RDD = df.toJavaRDD;
-    val dfRDDConversionTimeString = "DF to RDD Conversion Time: " + (System.nanoTime() - dfRDDConversionStart)/1000000 + "msec";
-
-//    val graphLoadStart = System.nanoTime();
-//    val graph = GraphLoader.edgeListFile(spark.sparkContext, options.getInputFiles()(0)).cache()
-//    val graphLoadTimeString = "Graph Load Time: " + (System.nanoTime() - graphLoadStart)/1000000 + "msec";
-//
-//    val graphRDDtoDFStart = System.nanoTime();
-//    val EdgeDF = spark.createDataFrame(graph.edges)
-//    val VertexDF = spark.createDataFrame(graph.vertices)
-//    val graphRDDtoDFTimeString = "Graph RDD to DF Time: " + (System.nanoTime() - graphRDDtoDFStart)/1000000 + "msec";
-//
-//    val graphParquetConversionStart = System.nanoTime();
-//    EdgeDF.write.parquet(s"${options.getInputFiles()(0)}.edge.parquet")
-//    VertexDF.write.parquet(s"${options.getInputFiles()(0)}.vertex.parquet")
-//    val graphParquetConversionTimeString = "Graph DF to Parquet Time: " + (System.nanoTime() - graphParquetConversionStart)/1000000 + "msec";
-//
-//    val graphParquetLoadStart = System.nanoTime();
-//    val EdgeRDD = spark.read.parquet(s"${options.getInputFiles()(0)}.edge.parquet")
-//      .rdd
-//      .map(row => Edge(row.getLong(0), row.getLong(1)))
-//    val VertexRDD = spark.read.parquet(s"${options.getInputFiles()(0)}.vertex.parquet")
-//      .rdd
-//      .map(row => (row.getLong(0), 1))
-//    val parquetGraph = new Graph(EdgeRDD, VertexRDD)
-
-    val HiBenchStart = System.nanoTime();
-    {
-      val lines = spark.sparkContext.textFile(options.getInputFiles()(0), 1)
-      val links = lines.map{ s =>
-        val elements = s.split("\\s+")
-        val parts = elements.slice(elements.length - 2, elements.length)
-        (parts(0), parts(1))
-      }.distinct().groupByKey().cache()
-      var ranks = links.mapValues(v => 1.0)
-    }
-    val HiBenchTimeString = "HiBench Graph Load Time: " + (System.nanoTime() - HiBenchStart)/1000000 + "msec";
-    val graphXStart = System.nanoTime();
     val graph = GraphLoader.edgeListFile(spark.sparkContext, options.getInputFiles()(0)).cache()
-    val graphXTimeString = "GraphX Graph Load Time: " + (System.nanoTime() - HiBenchStart)/1000000 + "msec";
+    s += step("[GraphX]Graph Load")
 
-    "Ran  ParquetConversion on " +
-      options.getInputFiles()(0) +
-      "\n\t" + DFLoadTimeString +
-      "\n\t" + RDDLoadTimeString +
-      "\n\t" + parquetConversionTimeString +
-      "\n\t" + parquetReadTimeString +
-      "\n\t" + dfRDDConversionTimeString +
-      "\n\t" + HiBenchTimeString +
-      "\n\t" + graphXTimeString
-//      "\n\t" + graphLoadTimeString +
-//      "\n\t" + graphRDDtoDFTimeString +
-//      "\n\t" + graphParquetConversionTimeString
+    val edgeDF = spark.createDataFrame(graph.edges)
+    s += step("[Edge]RDD->DF")
+
+    val edgeParquetName = s"${options.getInputFiles()(0)}.edge.parquet"
+    edgeDF.write.parquet(edgeParquetName)
+    s += step("[Edge]Saving Parquet")
+
+    val vertexDF = spark.createDataFrame(graph.edges)
+    s += step("[Vertex]RDD->DF")
+
+    val vertexParquetName = s"${options.getInputFiles()(0)}.vertex.parquet"
+    vertexDF.write.parquet(vertexParquetName)
+    s += step("[Vertex]Saving Parquet")
+
+    val edgeRDD = spark.read.parquet(edgeParquetName)
+      .rdd
+      .map(row => Edge[Long](row.getLong(0), row.getLong(1)))
+    s += step("[Edge]Restore RDD from parquet")
+
+    val vertexRDD = spark.read.parquet(vertexParquetName)
+      .rdd
+      .map(row => (row.getLong(0), 1))
+    s += step("[Vertex]Restore RDD from parquet")
+
+    val e = EdgeRDD.fromEdges(edgeRDD)
+    val v = VertexRDD.fromEdges(e, 1, 1)
+    s += step("Convert to EdgeRDD and VertexRDD")
+
+    val reConstructedGraph = GraphImpl.fromExistingRDDs(v, e)
+    s += step("[GraphX]Construct Graph with RDDs")
+
+    s
   }
 
   override def explain(): Unit = println(plainExplain())
