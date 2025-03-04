eaf8ff3e1f2f3ba96228ae0c66c478b8c4a0b0c5
Minsu Sun <20790969+minsusun@users.noreply.github.com>
2025-03-04 16:03:54 +0900

    feat(ParquetConversion): migrate to LogTrait

--- src/main/scala/com/ibm/crail/benchmarks/tests/ParquetConversion.scala
+++ src/main/scala/com/ibm/crail/benchmarks/tests/ParquetConversion.scala
@@ -1,23 +1,19 @@
 package com.ibm.crail.benchmarks.tests
 
-import com.ibm.crail.benchmarks.{ParseOptions, SQLTest}
+import com.ibm.crail.benchmarks.{LogTrait, ParseOptions, SQLTest}
 import org.apache.spark.SparkContext
 import org.apache.spark.graphx.impl.GraphImpl
-import org.apache.spark.graphx.{Edge, EdgeRDD, GraphLoader, GraphXHelper, VertexRDD, Graph}
+import org.apache.spark.graphx._
 import org.apache.spark.sql.{SaveMode, SparkSession}
 import org.apache.spark.storage.StorageLevel
 
-class ParquetConversion (val options: ParseOptions, spark:SparkSession) extends SQLTest(spark) {
-
-  private var s = "";
-  private var stepTime = System.nanoTime();
-
+class ParquetConversion (val options: ParseOptions, spark:SparkSession) extends SQLTest(spark) with LogTrait {
   def loader(sc: SparkContext, path: String): Graph[Int, Int] = {
     val edgeStorageLevel = StorageLevel.MEMORY_ONLY
     val vertexStorageLevel = StorageLevel.MEMORY_ONLY
 
     val lines = sc.textFile(path)
-    s += step("[AuxGraphLoader]Spark TextFile Read")
+    step("[AuxGraphLoader]Spark TextFile Read")
 
     val edges = lines.mapPartitionsWithIndex { (pid, iter) =>
       val h = new GraphXHelper()
@@ -39,45 +35,37 @@ class ParquetConversion (val options: ParseOptions, spark:SparkSession) extends 
       //        Iterator((pid, builder.toEdgePartition))
       Iterator((pid, h.method))
     }.persist(edgeStorageLevel).setName("GraphLoader.edgeListFile - edges (%s)".format(path))
-    s += step("[AuxGraphLoader]Edge Partition Build From Textfile")
+    step("[AuxGraphLoader]Edge Partition Build From Textfile")
 
     GraphImpl.fromEdgePartitions(edges, defaultVertexAttr = 1, edgeStorageLevel = edgeStorageLevel,
       vertexStorageLevel = vertexStorageLevel)
   }
 
-  def step(StepName: String): String = {
-    val now = System.nanoTime();
-    val info = s"\n\t\t\t\tStep '" + StepName + s"': ${(now - stepTime)/1000000} ms"
-    stepTime = now;
-    info
-  }
-
   override def execute(): String = {
-    s = "Ran Parquet Conversion on" + options.getInputFiles()(0)
     val graph = loader(spark.sparkContext, options.getInputFiles()(0)).cache()
 //    val graph = GraphLoader.edgeListFile(spark.sparkContext, options.getInputFiles()(0)).cache()
 //    s += step("[GraphX]Graph Load")
 
     val edgeDF = spark.createDataFrame(graph.edges)
-    s += step("[Edge]RDD->DF")
+    step("[Edge]RDD->DF")
 
     val edgeParquetName = s"${options.getInputFiles()(0)}.edge.parquet"
     edgeDF.write.mode(SaveMode.Overwrite).parquet(edgeParquetName)
-    s += step("[Edge]Saving Parquet")
+    step("[Edge]Saving Parquet")
 
     val edgeRDD = spark.read.parquet(edgeParquetName)
       .rdd
       .map(row => Edge[Long](row.getLong(0), row.getLong(1)))
-    s += step("[Edge]Restore RDD from parquet")
+    step("[Edge]Restore RDD from parquet")
 
     val e = EdgeRDD.fromEdges(edgeRDD)
     val v = VertexRDD.fromEdges(e, 1, 1)
-    s += step("Convert to EdgeRDD and VertexRDD")
+    step("Convert to EdgeRDD and VertexRDD")
 
     val reConstructedGraph = GraphImpl.fromExistingRDDs(v, e).cache()
-    s += step("[GraphX]Construct Graph with RDDs")
+    step("[GraphX]Construct Graph with RDDs")
 
-    s
+    "Ran Parquet Conversion on" + options.getInputFiles()(0) + logToString
   }
 
   override def explain(): Unit = println(plainExplain())
