661451ef9180cfcc0833b297c39e72dac806ed01
Minsu Sun <20790969+minsusun@users.noreply.github.com>
2025-03-04 16:32:30 +0900

    feat(ParquetConversion): use option to decide which graph loader to use

--- src/main/scala/com/ibm/crail/benchmarks/tests/ParquetConversion.scala
+++ src/main/scala/com/ibm/crail/benchmarks/tests/ParquetConversion.scala
@@ -1,50 +1,32 @@
 package com.ibm.crail.benchmarks.tests
 
 import com.ibm.crail.benchmarks.{LogTrait, ParseOptions, SQLTest}
-import org.apache.spark.SparkContext
 import org.apache.spark.graphx.impl.GraphImpl
-import org.apache.spark.graphx._
+import org.apache.spark.graphx.{AuxGraphLoader, Edge, EdgeRDD, Graph, GraphLoader, VertexRDD}
 import org.apache.spark.sql.{SaveMode, SparkSession}
-import org.apache.spark.storage.StorageLevel
 
 class ParquetConversion (val options: ParseOptions, spark:SparkSession) extends SQLTest(spark) with LogTrait {
-  def loader(sc: SparkContext, path: String): Graph[Int, Int] = {
-    val edgeStorageLevel = StorageLevel.MEMORY_ONLY
-    val vertexStorageLevel = StorageLevel.MEMORY_ONLY
-
-    val lines = sc.textFile(path)
-    step("[AuxGraphLoader]Spark TextFile Read")
-
-    val edges = lines.mapPartitionsWithIndex { (pid, iter) =>
-      val h = new GraphXHelper()
-      val b = h.instance
-
-//      val builder = new EdgePartitionBuilder[Int, Int]
-      iter.foreach { line =>
-        if (!line.isEmpty && line(0) != '#') {
-          val lineArray = line.split("\\s+")
-          if (lineArray.length < 2) {
-            throw new IllegalArgumentException("Invalid line: " + line)
-          }
-          val srcId = lineArray(0).toLong
-          val dstId = lineArray(1).toLong
-          //            builder.add(srcId, dstId, 1)
-          b.add(srcId, dstId, 1)
-        }
-      }
-      //        Iterator((pid, builder.toEdgePartition))
-      Iterator((pid, h.method))
-    }.persist(edgeStorageLevel).setName("GraphLoader.edgeListFile - edges (%s)".format(path))
-    step("[AuxGraphLoader]Edge Partition Build From Textfile")
-
-    GraphImpl.fromEdgePartitions(edges, defaultVertexAttr = 1, edgeStorageLevel = edgeStorageLevel,
-      vertexStorageLevel = vertexStorageLevel)
-  }
 
   override def execute(): String = {
-    val graph = loader(spark.sparkContext, options.getInputFiles()(0)).cache()
-//    val graph = GraphLoader.edgeListFile(spark.sparkContext, options.getInputFiles()(0)).cache()
-//    s += step("[GraphX]Graph Load")
+    var graph: Graph[Int, Int] = null
+    if (options.getAuxGraphLoader) {
+      val loader = new AuxGraphLoader()
+
+      graph = loader.edgeListFile(spark.sparkContext, options.getInputFiles()(0))
+      loader.step("[AuxGraphLoader]Load Graph")
+
+      graph = graph.cache()
+      loader.step("[AuxGraphLoader]Graph Cache")
+
+      concatLog(loader.logToString)
+      forceUpdate();
+    } else {
+      graph = GraphLoader.edgeListFile(spark.sparkContext, options.getInputFiles()(0))
+      step("[GraphX]Load Graph")
+
+      graph = graph.cache()
+      step("[GraphX]Graph Cache")
+    }
 
     val edgeDF = spark.createDataFrame(graph.edges)
     step("[Edge]RDD->DF")
