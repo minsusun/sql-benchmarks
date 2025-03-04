5712fe0f005ef1698ebe38e432fbd883352b612e
Minsu Sun <20790969+minsusun@users.noreply.github.com>
2025-03-04 15:56:58 +0900

    feat: add LogTrait for logging

---
+++ src/main/scala/com/ibm/crail/benchmarks/LogTrait.scala
@@ -0,0 +1,14 @@
+package com.ibm.crail.benchmarks
+
+trait LogTrait {
+  private var lastStepTime = System.nanoTime();
+  private var logString = ""
+
+  def step(StepName: String): Unit = {
+    val now = System.nanoTime();
+    logString += "\n\t\t\t\tStep '" + StepName + s"': ${(now - lastStepTime)/1000000} ms"
+    lastStepTime = now
+  }
+
+  def logToString: String = logString;
+}
